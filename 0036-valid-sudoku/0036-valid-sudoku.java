f0ad93ab59219e7b3b587ee1a24903de95e5206c
AliLZein <alielzeineducational@gmail.com>
2026-06-23 16:27:23 +0300

    0036-VALID-SUDOKU Stats: Time: 1 ms (99.97%), Space: 46.5 MB (58.48%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,23 +1,28 @@
-import java.util.HashSet;
-
 public class Solution {
     public boolean isValidSudoku(char[][] board) {
-        HashSet<String> seen = new HashSet<>();
+        boolean[][] rows = new boolean[9][10];
+        boolean[][] cols = new boolean[9][10];
+        boolean[][] boxes = new boolean[9][10];
         
         for (int r = 0; r < 9; r++) {
             for (int c = 0; c < 9; c++) {
-                char number = board[r][c];
+                char cell = board[r][c];
                 
-                if (number != '.') {
-                    
-                    String rowMarker = "row " + r + " has " + number;
-                    String colMarker = "col " + c + " has " + number;
-                    String boxMarker = "box " + (r / 3) + "-" + (c / 3) + " has " + number;
-                    
-                    if (!seen.add(rowMarker) || !seen.add(colMarker) || !seen.add(boxMarker)) {
-                        return false;
-                    }
+                if (cell == '.') {
+                    continue;
                 }
+                
+                int digit = cell - '0'; 
+                
+                int boxIndex = (r / 3) * 3 + (c / 3);
+                
+                if (rows[r][digit] || cols[c][digit] || boxes[boxIndex][digit]) {
+                    return false;
+                }
+                
+                rows[r][digit] = true;
+                cols[c][digit] = true;
+                boxes[boxIndex][digit] = true;
             }
         }
         
