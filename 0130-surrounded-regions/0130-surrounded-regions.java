692ba566624f9776434164fa41bbbb51d6b75721
AliLZein <alielzeineducational@gmail.com>
2026-07-10 12:11:36 +0300

    0130-SURROUNDED-REGIONS Stats: Time: 2 ms (72.65%), Space: 48 MB (10.81%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -4,11 +4,14 @@ class Solution
     {
         for(int i = 0 ; i < board.length ; i++)
         {
-            for(int j = 0 ; j < board[0].length ; j++)
-            {
-                boolean onedge = i == 0 || j == 0 || i == board.length - 1 || j == board[0].length - 1;
-                if(onedge && board[i][j] == 'O'){helper(board , i , j);}
-            }
+            if(board[i][0] == 'O'){ helper(board , i , 0);}
+            if(board[i][board[0].length - 1] == 'O') helper(board , i , board[0].length - 1);
+        }
+
+        for(int j = 1 ; j < board[0].length - 1 ; j++)
+        {
+            if(board[0][j] == 'O') helper(board , 0 , j);
+            if(board[board.length - 1][j] == 'O') helper(board , board.length - 1, j);
         }
         for(int k = 0 ; k < board.length ; k++)
         {
