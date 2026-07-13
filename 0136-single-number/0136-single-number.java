820fa6c04075e1b7bb58881bc2a29fa3107d6136
AliLZein <alielzeineducational@gmail.com>
2026-07-13 19:45:25 +0300

    0136-SINGLE-NUMBER Stats: Time: 1 ms (99.97%), Space: 46.7 MB (82.26%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -2,15 +2,12 @@ class Solution
 {
     public int singleNumber(int[] nums) 
     {
-        if(nums.length == 1){return nums[0];}
-
-        Arrays.sort(nums);
-
-        for(int i = 0 ; i < nums.length - 1; i += 2)
-        {
-            if(nums[i] != nums[i + 1]){return nums[i];}
+        int result = 0;
+        
+        for (int num : nums) {
+            result = result ^ num;
         }
-
-        return nums[nums.length - 1];
+        
+        return result;
     }
 }
